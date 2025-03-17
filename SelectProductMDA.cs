403ec8e517b5442b66518efa60e0758ea149bad8
DieroKreator <qtechwired@gmail.com>
2025-03-17 17:42:48 -0300

    Add test for product selection and cart validation in SelectProductMDA

--- SelectProductMDA.cs
+++ SelectProductMDA.cs
@@ -1,6 +1,7 @@
 using OpenQA.Selenium.Appium;
 using OpenQA.Selenium.Appium.Android;
 using OpenQA.Selenium.Appium.Enums;
+using OpenQA.Selenium.Appium.MultiTouch;
 
 namespace appium;
 
@@ -38,5 +39,49 @@ public class SelectProductMDA
     {
         if (driver == null) return; // Se não tem o driver ativo, apenas termine o script
         driver.Quit();
+        driver.Dispose();
+    }
+
+    [Test] // Teste em si
+    public void SelecionarProdutoMDA()
+    {
+        // É importante começar com uma linha que aguarda carregar algum elemento da página inicial do app 
+        Assert.That(driver.FindElement(MobileBy.AccessibilityId("App logo and name")).Displayed, Is.True);
+
+        // Clicar no produto mochila
+        driver.FindElement(MobileBy.AccessibilityId("Sauce Labs Backpack")).Click();
+
+        // Verificar o nome do produto na tela do produto
+        String tituloProduto = driver.FindElement(MobileBy.Id("com.saucelabs.mydemoapp.android:id/productTV")).Text;
+        Assert.That(tituloProduto, Is.EqualTo("Sauce Labs Backpack"));
+
+        // Exemplo com 1 linha
+        // Assert.That(driver.FindElement(MobileBy.Id("com.saucelabs.mydemoapp.android:id/productTV")).Text, Is.EqualTo("Sauce Labs Back Packs")); 
+
+        // Verificar o preco do produto
+        String precoProduto = driver.FindElement(MobileBy.Id("com.saucelabs.mydemoapp.android:id/priceTV")).Text;
+        Assert.That(precoProduto, Is.EqualTo("$ 29.99"));
+
+        // Arrasta para cima
+        TouchAction touchAction = new TouchAction(driver); // instancia objeto para reproduzir gestos
+        touchAction.Press(600, 1700);
+        touchAction.MoveTo(600, 700);
+        touchAction.Release();
+        touchAction.Perform();
+
+        // Adicionar no carrinho
+        driver.FindElement(MobileBy.AccessibilityId("Tap to add product to cart")).Click();
+
+        // Clicar no carrinho
+        driver.FindElement(MobileBy.Id("com.saucelabs.mydemoapp.android:id/cartTV")).Click();
+
+        // Validar o nome do produto no carrinho
+        tituloProduto = driver.FindElement(MobileBy.Id("com.saucelabs.mydemoapp.android:id/cartIV")).Text;
+        Assert.That(tituloProduto, Is.EqualTo("Sauce Labs Backpack"));
+
+        // Validar o preco do produto no carrinho
+        precoProduto = driver.FindElement(MobileBy.Id("com.saucelabs.mydemoapp.android:id/priceTV")).Text;
+        Assert.That(precoProduto, Is.EqualTo("$ 29.99"));
+
     }
 }
